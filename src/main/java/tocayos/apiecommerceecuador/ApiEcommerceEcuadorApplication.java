2469d58686be53611f285a18efae5cd9eb3d47b3
anderk222 <anderk222@gmail.com>
2023-09-16 11:51:27 -0500

    remove exclude autoconfig mongo data

--- src/main/java/tocayos/apiecommerceecuador/ApiEcommerceEcuadorApplication.java
+++ src/main/java/tocayos/apiecommerceecuador/ApiEcommerceEcuadorApplication.java
@@ -2,14 +2,10 @@ package tocayos.apiecommerceecuador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
-MongoAutoConfiguration.class,
-MongoDataAutoConfiguration.class
-}
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}
+       
 )
 public class ApiEcommerceEcuadorApplication {
 
